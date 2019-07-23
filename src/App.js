d0badc1d7d716f24c77e1a263ac29253e242df46
Andrew Elliott <mtgelliott15@gmail.com>
2019-07-22 20:07:38 -0500

    Build input child and parent components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      title: 'Marvel Search Engine 2.0'
+    };
+  }
+  render(){
+    return(
+      <div>
+        <h1 className="App-header">{this.state.title}</h1>
+        <div className="do-it-all">
+          <InputKickoff className="api-input" input={this.state.inputValue} handleChange={this.handleChange}/>
+        </div>
+      </div>
+    )
+  }
+}
+
+class InputKickoff extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      input: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.apiKickoff = this.apiKickoff.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      inputValue: event.target.value
+      });
+    };
+
+  apiKickoff({this.state.input}){
+
+  }
+  render() {
+    return (
+      <div>
+        <input value={this.props.input} onChange={this.props.handleChange}/>
+        <button className="api-kickoff">Search the Database</button>
+      </div>
+    );
+  }
 }
 
 export default App;

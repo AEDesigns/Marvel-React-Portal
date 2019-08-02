486bd05acac51c3d7ae68bf5fb9ee5dbe1b6f801
Andrew Elliott <mtgelliott15@gmail.com>
2019-08-01 19:45:04 -0500

    Fix setState bug. Remove test btn

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,19 +10,20 @@ class CustomCard extends React.Component {
         this.toggleCSS = this.toggleCSS.bind(this);
     }
     toggleCSS() {
-        this.setState = {
+        this.setState ({
             display: !this.state.display
-        }
-    }
+        });
+    };
+    
     render() {
         return (
             <div>
-                <div class="card" style={{ width: "15rem" }} className="EventsDiv">
+                <div class="card" style={{ width: "18rem" }} className="EventsDiv">
                     <img class="card-img-top" src={this.props.img + "/standard_medium.jpg"} alt={"The Cover Photo for the Comic series " + this.props.title} />
                     <div class="card-body">
                         <h5 class="card-title">{this.props.title}</h5>
                         <button type="button" class="btn btn-primary" onClick={this.toggleCSS}>View Description</button>
-                        <p class={`card-text custom-overflow${this.state.display ? '-2' : ''}`}>{this.props.body}</p>
+                        <p class={`card-text ${this.state.display ? 'custom-overflow-2' : 'custom-overflow'}`}>{this.props.body}</p>
                     </div>
                 </div>
             </div>
